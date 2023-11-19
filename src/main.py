e06566b1c8feefd63fe9c4c0fd9003742df19929
John Nipp <jcnipp@berkeley.edu>
2023-11-18 22:19:09 -0800

    Made the speech-to-text librarian

--- src/main.py
+++ src/main.py
@@ -1,12 +1,44 @@
 from pathlib import Path
 import os
 import openai
-from dotenv import load_dotenv
-load_dotenv()
-openai.api_key = os.getenv('OPENAI_API_KEY')
-with open(Path.cwd() / "src" / "lost_debit_card.wav", "rb") as audio_file:
-    transcript = openai.Audio.transcribe("whisper-1", audio_file)
-    print(transcript)
+
+# import whisper
+from openai import OpenAI
+from OPEN_AI_KEY import OPENAI_API_KEY
+# openai.api_key = OPENAI_API_KEY
+client = OpenAI(
+    api_key = OPENAI_API_KEY
+)
+client2 = OpenAI(
+    api_key = OPENAI_API_KEY
+)
+
+# Main code
+visitor_inquiry = input("""Hello, please tell me a little bit about yourself",
+so that I can help you pick out a book that you might like. Share an audio with
+me. \nJust enter the file from the current directory\n""")
+
+audio_file= open(visitor_inquiry, "rb")
+transcript = client2.audio.transcriptions.create(
+  model="whisper-1",
+  file=audio_file,
+  response_format="text"
+)
+
+response = client.chat.completions.create(
+    model = 'gpt-3.5-turbo',
+    messages = [
+        {   "role": "system", "content": """You are a virtual librarian, who is
+            helping visitors find a book that they might enjoy reading. You are
+            knowledgeable and give reccomendations based on personal preferences
+            as well as their character traits. """
+        },
+        {"role": "user", "content": transcript}
+    ],
+    temperature = .7,
+    max_tokens = 1000
+)
+print(response.choices[0].message.content)
 
 """to enable your own recording on a non codespaces
 freq = 44100
@@ -15,7 +47,7 @@ import sounddevice as sd
 from scipy.io.wavfile import write
 recording = sd.rec(int(duration * freq),
                    samplerate=freq, channels=2)
- 
+
 sd.wait()
 write("my_audio.wav", freq, recording)
 """
